dfcbe3b24b543a9b6206747f5dad809e799edc51
Can <150291155+canlotus@users.noreply.github.com>
2025-02-18 19:32:31 +0300

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject gamePanel; // Oyun ekranını temsil eden panel
+    public GameObject gamePanel; 
 
     void Start()
     {
-        // Oyun ekranını başlangıçta kapalı tut
+        
         gamePanel.SetActive(false);
     }
 
     public void StartGame()
     {
-        // Play butonuna basıldığında oyun ekranını aç
+        
         gamePanel.SetActive(true);
     }
 
     public void CloseGamePanel()
     {
-        // Gerekirse oyun ekranını kapatmak için bir fonksiyon
+        
         gamePanel.SetActive(false);
     }
 }
